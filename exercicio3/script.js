22247a74d2063b5f4be9a5b6f031612b96d33158
wangunderline <wangwangrian@gmail.com>
2022-10-18 21:17:59 -0300

    exercicios-rian

---
+++ exercicio3/script.js
@@ -0,0 +1,82 @@
+const nome = "Nome completo"
+const askNome = prompt("Nome completo")
+
+const data = "Data de nascimento"
+const askData = prompt("Data de nascimento")
+
+let endereco = "Endereço"
+let askEndereco = prompt("Endereço")
+
+const cpf = "CPF"
+const askCpf = Number(prompt("cpf"))
+
+const escolaridade = "escolaridade"
+const askEscolaridade = prompt("escolaridade")
+
+let temCnh = "possui cnh?"
+let askTemCnh = prompt("possui cnh?")
+
+let filhos = "quantos filhos?"
+let askFilhos = prompt("quantos filhos?")
+
+let cargo = "cargo atual"
+let askCargo = prompt("cargo atual")
+
+let salario = "salário"
+let askSalario = Number(prompt("salário"))
+
+let comissao = "recebe comissão?"
+let askComissao = prompt("recebe comissão?")
+
+const admissao = "ano de admissão"
+const askAdmissao = Number(prompt("ano de admissão"))
+
+
+console.log(nome)
+console.log(askNome)
+
+console.log(data)
+console.log(askData)
+
+console.log(endereco)
+console.log(askEndereco)
+
+console.log(cpf)
+console.log(askCpf)
+
+console.log(escolaridade)
+console.log(askEscolaridade)
+
+console.log(temCnh)
+console.log(askTemCnh)
+
+console.log(filhos)
+console.log(askFilhos)
+
+console.log(cargo)
+console.log(askCargo)
+
+console.log(salario)
+console.log(askSalario)
+
+console.log(comissao)
+console.log(askComissao)
+
+console.log(admissao)
+console.log(askAdmissao)
+
+
+console.log(typeof nome)
+console.log(typeof data)
+console.log(typeof endereco)
+console.log(typeof cpf)
+console.log(typeof escolaridade)
+console.log(typeof temCnh)
+console.log(typeof filhos)
+console.log(typeof cargo)
+console.log(typeof salario)
+console.log(typeof comissao)
+console.log(typeof admissao)
+
+console.log(nome, askNome, data, askData, endereco, askEndereco, cpf, askCpf, escolaridade, askEscolaridade, temCnh, 
+askTemCnh, filhos, askFilhos, cargo, askCargo, salario, askSalario, comissao, askComissao, admissao, askAdmissao)
